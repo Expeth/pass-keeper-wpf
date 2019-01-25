ad1f196baa691dfe17b134eb28105da0f6e31ddc
Valeriy Kovetskiy <kovetskiy.valeriy@gmail.com>
2019-01-25 19:51:00 +0200

    Added interface IRecord

---
+++ PassKeeper_WPF/Infrastructure/IRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassKeeper_WPF
+{
+    public interface IRecord
+    {
+        string Title { get; set; }
+        string Note { get; set; }
+        string WebsiteName { get; set; }
+        string Username { get; set; }
+        string Password { get; set; }
+    }
+}
